52e98a00de2bc745e9dd3b8cc7e0a7c3655dd2aa
wanggang <514260663@qq.com>
2020-02-24 18:38:46 +0800

    fix

--- app/src/main/java/org/fireking/ap/custom/nested/NestedSample3Child.java
+++ app/src/main/java/org/fireking/ap/custom/nested/NestedSample3Child.java
@@ -3,61 +3,85 @@ package org.fireking.ap.custom.nested;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.view.NestedScrollingChild2;
+import androidx.core.view.NestedScrollingChildHelper;
+import androidx.core.view.ViewCompat;
+
+import static androidx.core.view.ViewCompat.TYPE_TOUCH;
 
 public class NestedSample3Child extends LinearLayout implements NestedScrollingChild2 {
 
+    private NestedScrollingChildHelper nestedScrollingChildHelper;
+    private int mLastTouchX;
+    private int mLastTouchY;
+
     public NestedSample3Child(Context context) {
-        super(context);
-        init();
+        this(context, null);
     }
 
     public NestedSample3Child(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
+        this(context, attrs, 0);
     }
 
     public NestedSample3Child(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public NestedSample3Child(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init();
-    }
-
-    private void init() {
         setOrientation(VERTICAL);
+        nestedScrollingChildHelper = new NestedScrollingChildHelper(this);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mLastTouchX = (int) event.getRawX();
+        mLastTouchY = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startNestedScroll(ViewCompat.SCROLL_AXIS_VERTICAL, TYPE_TOUCH);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //询问父组件是否消费
+                if (dispatchNestedPreScroll()) {
+
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
     public boolean startNestedScroll(int axes, int type) {
-        return false;
+        return nestedScrollingChildHelper.startNestedScroll(axes, type);
     }
 
     @Override
     public void stopNestedScroll(int type) {
-
+        nestedScrollingChildHelper.stopNestedScroll(type);
     }
 
     @Override
     public boolean hasNestedScrollingParent(int type) {
-        return false;
+        return nestedScrollingChildHelper.hasNestedScrollingParent(type);
     }
 
     @Override
     public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, @Nullable int[] offsetInWindow, int type) {
-        return false;
+        return nestedScrollingChildHelper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow, type);
     }
 
     @Override
     public boolean dispatchNestedPreScroll(int dx, int dy, @Nullable int[] consumed, @Nullable int[] offsetInWindow, int type) {
-        return false;
+        return nestedScrollingChildHelper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow, type);
     }
 }
--- app/src/main/java/org/fireking/ap/custom/nested/NestedSample3Parent.java
+++ app/src/main/java/org/fireking/ap/custom/nested/NestedSample3Parent.java
@@ -1,87 +1,100 @@
 package org.fireking.ap.custom.nested;
 
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.core.view.NestedScrollingParent2;
 import androidx.core.view.NestedScrollingParentHelper;
-
-import org.fireking.ap.R;
+import androidx.core.view.ViewCompat;
 
 public class NestedSample3Parent extends LinearLayout implements NestedScrollingParent2 {
 
-    private NestedSample3Child nested_sample3_child;
+    private View headerView;
 
-    private int mNestedSampleChildHeight;
+    private int mHeaderViewHeight;
 
-    private NestedScrollingParentHelper helper = new NestedScrollingParentHelper(this);
+    private NestedScrollingParentHelper nestedScrollingParentHelper;
 
     public NestedSample3Parent(Context context) {
-        super(context);
-        init();
+        this(context, null);
     }
 
     public NestedSample3Parent(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
+        this(context, attrs, 0);
     }
 
     public NestedSample3Parent(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public NestedSample3Parent(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init();
-    }
-
-    private void init() {
         setOrientation(VERTICAL);
+        nestedScrollingParentHelper = new NestedScrollingParentHelper(this);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    @Override
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
+        nestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes, type);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        nested_sample3_child = findViewById(R.id.nested_sample3_child);
+        headerView = getChildAt(0);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        mNestedSampleChildHeight = nested_sample3_child.getMeasuredHeight();
+        mHeaderViewHeight = headerView.getMeasuredHeight();
+        int parentHeightMeasureSpec = View.MeasureSpec.makeMeasureSpec(getMeasuredHeight() + mHeaderViewHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, parentHeightMeasureSpec);
     }
 
-    @Override
-    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
-        return true;
-    }
-
-    @Override
-    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
-        helper.onNestedScrollAccepted(child, target, type);
-    }
 
     @Override
     public void onStopNestedScroll(@NonNull View target, int type) {
-        helper.onStopNestedScroll(target, type);
+        nestedScrollingParentHelper.onStopNestedScroll(target, type);
     }
 
     @Override
     public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
-
     }
 
     @Override
     public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
-        Log.e("info", "-->" + target.getScaleY() + ", " + getScaleY() + ", " + dy);
+        boolean hideTop = dy > 0 && getScrollY() < mHeaderViewHeight;
+        boolean showTop = dy < 0 && getScrollY() >= 0 && !target.canScrollVertically(-1);
+        if (hideTop || showTop) {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+        }
+    }
+
+    @Override
+    public int getNestedScrollAxes() {
+        return nestedScrollingParentHelper.getNestedScrollAxes();
+    }
+
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        if (y < 0) {
+            y = 0;
+        }
+        if (y > mHeaderViewHeight) {
+            y = mHeaderViewHeight;
+        }
+        super.scrollTo(x, y);
     }
 }
