b5fbf4f5172950f064cc4bddca69dca6a1353192
王刚 <514260663@qq.com>
2020-11-12 21:42:37 +0800

    fix

--- app/src/main/java/org/fireking/ap/custom/recyclerview/decoration/ItemDecorationV1Activity.kt
+++ app/src/main/java/org/fireking/ap/custom/recyclerview/decoration/ItemDecorationV1Activity.kt
@@ -3,6 +3,9 @@ package org.fireking.ap.custom.recyclerview.decoration
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_item_decoration_v1.*
 import org.fireking.ap.R
@@ -22,12 +25,21 @@ class ItemDecorationV1Activity : AppCompatActivity() {
         setContentView(R.layout.activity_item_decoration_v1)
 
         rv_content_list.layoutManager = LinearLayoutManager(this)
-        rv_content_list.addItemDecoration(
-            ItemDecorationV1(
-                this,
-                5F,
-                3,
-                R.layout.decoration_space_layout
+        val stickyView = LayoutInflater.from(this)
+            .inflate(R.layout.decoration_space_layout, rv_content_list, false)
+        stickyView.findViewById<ImageView>(R.id.iv_launcher).setOnClickListener {
+            Toast.makeText(this, "点击了图标", Toast.LENGTH_SHORT).show()
+        }
+        val itemDecoration = PowerStickyItemDecoration(
+            context = this,
+            stickyView = stickyView,
+            stickyPosition = 2
+        )
+        rv_content_list.addItemDecoration(itemDecoration)
+        rv_content_list.addOnItemTouchListener(
+            PowerStickyItemTouchListener(
+                rv_content_list,
+                itemDecoration
             )
         )
         rv_content_list.adapter = ItemDecorationV1Adapter()
